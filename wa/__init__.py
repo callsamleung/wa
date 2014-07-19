3a4cffa741cd987d21b557254229accb3975fb59
callsamleung <callsamleung@gmail.com>
2014-07-19 16:28:39 +0800

    run flask_user mini

--- wa/__init__.py
+++ wa/__init__.py
@@ -9,9 +9,16 @@ __all__ = ('PluginInterface',
         'create_app',
         )
 
+class ConfigClass(object):
+    # Configure Flask
+    SECRET_KEY = 'THIS IS AN INSECURE SECRET'             # Change this for production!!!
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///minimal_app.db'  # Use Sqlite file db
+    CSRF_ENABLED = True
+
 def create_app(config_file):
     app = App(__name__)
-    app.config.from_pyfile(config_file)
-    app.init_plugins()
+    #app.config.from_pyfile(config_file)
+    app.config.from_object(__name__+'.ConfigClass')
+    #app.init_plugins()
     return app
 
