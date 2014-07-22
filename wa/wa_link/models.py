78ddf314544b77a4511878d2c0bb5d16c319e689
callsamleung <callsamleung@gmail.com>
2014-07-22 15:00:00 +0800

    include wa_link package

---
+++ wa/wa_link/models.py
@@ -0,0 +1,6 @@
+#coding:utf-8
+
+class WAModelLink():
+    def __init__(self):
+        self.link = 'http://baidu.com'
+    
---
+++ wa/wa_link/views.py
@@ -0,0 +1,12 @@
+#coding:utf-8
+
+from models import WAModelLink
+class WAViewLink():
+    def __init__(self):
+        pass
+    def get_link(self, lid):
+        return WAModelLink()
+
+
+    
+
